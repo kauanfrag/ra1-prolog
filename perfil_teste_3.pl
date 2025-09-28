bf74c3c59cd6df99b748784d8add4ebbdc8ee4b6
kauanfrag <163474592+kauanfrag@users.noreply.github.com>
2025-09-28 19:22:46 -0300

    Add files via upload
    perfil teste 3

---
+++ perfil_teste_3.pl
@@ -0,0 +1,16 @@
+% teste ciberseguranca
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, n).
+resposta(5, s).
+resposta(6, s).
+resposta(7, n).
+resposta(8, n).
+resposta(9, s).
+resposta(10, n).
+resposta(11, n).
+resposta(12, n).
+resposta(13, n).
+resposta(14, n).
+resposta(15, n).
