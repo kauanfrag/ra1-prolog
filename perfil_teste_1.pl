eb00c2960e83e76dcf1227c4bcc7e6bbb4ddb008
Lucas Maciel Cardoso <124613845+LucasC64@users.noreply.github.com>
2025-09-28 10:44:23 -0300

    Add files via upload

---
+++ perfil_teste_1.pl
@@ -0,0 +1,16 @@
+% teste ciencia de dados
+resposta(1, s).
+resposta(2, s).
+resposta(3, s).
+resposta(4, n).
+resposta(5, n).
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, n).
+resposta(10, n).
+resposta(11, n).
+resposta(12, n).
+resposta(13, n).
+resposta(14, n).
+resposta(15, n).
---
+++ perfil_teste_2.pl
@@ -0,0 +1,16 @@
+% teste front-end
+resposta(1, n).
+resposta(2, n).
+resposta(3, n).
+resposta(4, n).
+resposta(5, n).
+resposta(6, n).
+resposta(7, n).
+resposta(8, n).
+resposta(9, n).
+resposta(10, n).
+resposta(11, n).
+resposta(12, n).
+resposta(13, n).
+resposta(14, s).
+resposta(15, s).
