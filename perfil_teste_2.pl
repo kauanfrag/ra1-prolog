773e9a90481c3f1f940e0ea215d82aaef808ba25
kauanfrag <163474592+kauanfrag@users.noreply.github.com>
2025-09-28 21:33:37 -0300

    Update perfil_teste_2.pl

--- perfil_teste_2.pl
+++ perfil_teste_2.pl
@@ -4,7 +4,7 @@ resposta(2, n).
 resposta(3, n).
 resposta(4, n).
 resposta(5, n).
-resposta(6, s).
+resposta(6, n).
 resposta(7, n).
 resposta(8, n).
 resposta(9, n).
@@ -14,4 +14,5 @@ resposta(12, n).
 resposta(13, n).
 resposta(14, s).
 resposta(15, s).
+
 
